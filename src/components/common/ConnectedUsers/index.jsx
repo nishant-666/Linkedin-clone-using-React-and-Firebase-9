7e99b477d9e4e48258add14a05145093368c7127
Nishant <nishants440@gmail.com>
2023-03-01 20:59:59 +0530

    Designing the Connections Page

--- src/components/common/ConnectedUsers/index.jsx
+++ src/components/common/ConnectedUsers/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
 import { getConnections } from "../../../api/FirestoreAPI";
 
 export default function ConnectedUsers({ user, getCurrentUser, currentUser }) {
@@ -9,9 +10,15 @@ export default function ConnectedUsers({ user, getCurrentUser, currentUser }) {
   return isConnected ? (
     <></>
   ) : (
-    <div className="grid-child" onClick={() => getCurrentUser(user.id)}>
-      <p>{user.name}</p>
-      <p>{user.headline}</p>
+    <div className="grid-child">
+      <img src={user.imageLink} />
+      <p className="name">{user.name}</p>
+      <p className="headline">{user.headline}</p>
+
+      <button onClick={() => getCurrentUser(user.id)}>
+        <AiOutlineUsergroupAdd size={20} />
+        Connect
+      </button>
     </div>
   );
 }
