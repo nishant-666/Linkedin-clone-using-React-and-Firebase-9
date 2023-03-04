d0e3274dd96394e2dcd926cfc0808e2a9a038ab1
Nishant <nishants440@gmail.com>
2023-03-05 01:25:32 +0530

    Final Changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,14 +7,7 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBbPq_uA3lXjole4YoPBpNNQz5FHd0YD8k",
-  authDomain: "linkedin-clone-a077e.firebaseapp.com",
-  projectId: "linkedin-clone-a077e",
-  storageBucket: "linkedin-clone-a077e.appspot.com",
-  messagingSenderId: "24812638518",
-  appId: "1:24812638518:web:8eb2ee581841c837a1a397",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
